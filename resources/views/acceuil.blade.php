34a36c6f1f5b7d31052a5f2db5e121e92501d0e1
thierno.barry <thiernoalhassanebarry6@gmail.com>
2018-08-15 12:42:58 +0000

    test123444

--- resources/views/acceuil.blade.php
+++ resources/views/acceuil.blade.php
@@ -18,14 +18,14 @@
 
      -->
 
-    <link rel="stylesheet" href="{{asset('resources/assets/acceuillogin/styles/bootstrap4/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('acceuillogin/styles/bootstrap4/bootstrap.min.css')}} ">
     <link rel="stylesheet" href="{{asset('resources/assets/acceuillogin/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css')}}">
     <link rel="stylesheet" href="{{asset('resources/assets/acceuillogin/plugins/OwlCarousel2-2.2.1/owl.carousel.css')}}">
     <link rel="stylesheet" href="{{asset('resources/assets/acceuillogin/plugins/OwlCarousel2-2.2.1/owl.theme.default.css')}}">
     <link rel="stylesheet" href="{{asset('resources/assets/acceuillogin/plugins/OwlCarousel2-2.2.1/animate.css')}}">
     <link rel="stylesheet" href="{{asset('resources/assets/acceuillogin/plugins/slick-1.8.0/slick.css')}}">
-    <link rel="stylesheet" href="{{asset('resources/assets/acceuillogin/styles/main_styles.css')}}">
-    <link rel="stylesheet" href="{{asset('resources/assets/acceuillogin/styles/responsive.css')}}">
+    <link rel="stylesheet" href="{{ asset('acceuillogin/styles/main_styles.css')}} ">
+    <link rel="stylesheet" href="{{ asset('acceuillogin/styles/responsive.css')}} ">
 
 
 </head>
