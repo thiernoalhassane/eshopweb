d30dcbb75e6f4150c4061cd1e05f05f3e3ea9448
thierno.barry <thiernoalhassanebarry6@gmail.com>
2018-08-17 20:59:20 +0000

    Soucis HTTPS part 5
    modification du fichier .env  avec ajout de la commande php artisan config:clear et modifcation du fichier Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,10 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') == 'production') {
+        if(env('REDIRECT_HTTPS'))
+        {
             \URL::forceScheme('https');
-
         }
+
+        Schema::defaultStringLength(191);
     }
 
     /**
