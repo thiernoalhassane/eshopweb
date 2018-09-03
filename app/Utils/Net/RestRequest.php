a9563d9d67ddba86a6d1ebfc05489f25cca8bad5
fatigba72 <fatigba72@gmail.com>
2018-09-03 19:54:04 +0000

    feature(REST Request): implémentation de la méthode PUT.

--- app/Utils/Net/RestRequest.php
+++ app/Utils/Net/RestRequest.php
@@ -219,6 +219,28 @@ class RestRequest
 
         return Cache::get("user:{$user_id}:items") ;
     }
+
+    /**
+     * Fait une requête HTTP::PUT
+     * @param string $path
+     * @param string $body_type 'multipart', 'form_params', 'json'
+     * @param array $data
+     * @param array $query
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \InvalidArgumentException si $body_type ne correspond à aucune valeur prédéfinie.
+     */
+    public function put(string $path, string $body_type, array $data, array $query = [])
+    {
+        $body_types = ['multipart', 'form_params', 'json'] ;
+        if(!in_array($body_type, $body_types))
+        {
+            throw new InvalidArgumentException("body_type doit avoir une des valeurs suivante: multipart, form_params ou json") ;
+        }
+        $params["query"]=$query;
+        $params[$body_type]=$data;
+        //dd($params) ;
+        return $this->httpClient->request("PUT", $path, $params) ;
+    }
 }
 
 /**
