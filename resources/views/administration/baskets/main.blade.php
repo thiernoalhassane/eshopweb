c0ad46a301d2d3d1d5f3b50d481f84b245ec44c2
fatigba72 <fatigba72@gmail.com>
2018-09-14 21:12:29 +0000

    features(panier): ajout d'un template pour un affichage dans le backend.

---
+++ resources/views/administration/baskets/main.blade.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jordy
+ * Date: 14/09/18
+ * Time: 16:47
+ *
+ * Page d'affichage de la liste des paniers enregistrés.
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Liste Des Produits</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    {!! HTML::style('administration/bootstrap/css/bootstrap.min.css') !!}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    {!! HTML::style('administration/plugins/datatables/dataTables.bootstrap.css') !!}
+    {!! HTML::style('administration/dist/css/AdminLTE.min.css') !!}
+    {!! HTML::style('administration/dist/css/skins/_all-skins.min.css') !!}
+
+
+</head>
+<body class="hold-transition skin-blue fixed">
+<div class="wrapper">
+
+@include('administration/hearder')
+<!-- Left side column. contains the logo and sidebar -->
+@include('administration/menu')
+
+<!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <div class="alert-warning">
+            @if(isset($errors))
+                @foreach($errors->all() as $error)
+                    <span class="">{{ $error }}</span>
+                @endforeach
+            @endif
+        </div>
+        <!-- Content Header (Page header) -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <!-- Message lié à la mise à jour d'un produit -->
+                @if(Session::has('error_while_update_item'))
+                    @include('partials/error', ['type' => 'warning', 'message' => Session::get('error_while_update_item') ])
+                @elseif(Session::has('succes_while_update_item'))
+                    @include('partials/error', ['type' => 'success', 'message' => Session::get('succes_while_update_item') ])
+                @endif
+
+                <div class="col-xs-12">
+                    <div class="box box-primary">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Liste de vos produits</h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            @if($user_baskets != null)
+
+                            @else
+                                <div class="alert alert-primary">
+                                    <div class="text-muted text-center" style="font-family: Cousine; font-size: 2em">
+                                        Vous n'avez aucun panier enregistré !
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+                <!-- /.col -->
+            </div>
+
+
+        </section>
+        <!-- /.content -->
+        <!-- modal for item update form -->
+        <div class="container">
+            <div class="modal fade" role="dialog" id="itemUpdateFormModal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="bg-primary modal-header">
+                            <button type="button" class="close" data-dismiss="modal">X</button>
+                            <div class="modal-title">
+                                <h3 class="text-center">Modification</h3>
+                            </div>
+
+                        </div>
+                        <div class="modal-body">
+                            <div id="itemUpdateForm">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <!-- /.content-wrapper -->
+
+    @include('administration/footer')
+    @include('administration/settings')
+
+</div>
+<!-- ./wrapper -->
+{!! HTML::script('administration/plugins/jQuery/jquery-2.2.3.min.js') !!}
+{!! HTML::script('administration/bootstrap/js/bootstrap.min.js') !!}
+{!! HTML::script('administration/plugins/datatables/jquery.dataTables.min.js') !!}
+{!! HTML::script('administration/plugins/datatables/dataTables.bootstrap.min.js') !!}
+{!! HTML::script('administration/plugins/slimScroll/jquery.slimscroll.min.js') !!}
+{!! HTML::script('administration/plugins/fastclick/fastclick.js') !!}
+{!! HTML::script('administration/dist/js/app.min.js') !!}
+{!! HTML::script('administration/dist/js/demo.js') !!}
+{!! HTML::script('js/adminpanel.js') !!}
+<script>
+    $(function () {
+        $("#example1").DataTable();
+    });
+</script>
+
+</body>
+</html>
