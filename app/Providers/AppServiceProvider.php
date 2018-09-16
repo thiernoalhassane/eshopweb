3817a6a1e1bb47b8a98b9b498f9904ae2ab59376
thierno.barry <thiernoalhassanebarry6@gmail.com>
2018-09-16 12:53:49 +0000

    VERSION 1.0 Correction Bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,16 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') !== 'local') {
+
             if (env('REDIRECT_HTTPS'))
             {
                 \URL::forceScheme('https');
             }
 
             Schema::defaultStringLength(191);
-        }
-
-
+        
 
     }
 
