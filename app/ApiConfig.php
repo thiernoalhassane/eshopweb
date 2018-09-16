ecd3fb9ae0ec529ee1cea5e699c549a6b68cb7e4
thierno.barry <thiernoalhassanebarry6@gmail.com>
2018-09-16 13:20:08 +0000

    VERSION 1.0 Correction Bug Changement lien webservice

--- app/ApiConfig.php
+++ app/ApiConfig.php
@@ -11,17 +11,16 @@ namespace App;
 
 class ApiConfig
 {
-    protected $client_id = "5b26340139e44b2a9bd4ee30";
-    protected $url_auth = "https://eshopdocker-opentrade.193b.starter-ca-central-1.openshiftapps.com/eshoprest/api/o";
-    protected $url_rest = "https://eshopdocker-opentrade.193b.starter-ca-central-1.openshiftapps.com/eshoprest/";
 
+
+    protected $client_id = "5b26340139e44b2a9bd4ee30";
+    protected $url_auth = "https://nameless-tor-45908.herokuapp.com/api/o";
+    protected $url_rest = "https://desolate-castle-82146.herokuapp.com/";
 
     /* protected $client_id = "5b26340139e44b2a9bd4ee30";
      protected $url_auth = "https://eshopdocker-opentrade.193b.starter-ca-central-1.openshiftapps.com/eshoprest/api/o";
      protected $url_rest = "https://eshopdocker-opentrade.193b.starter-ca-central-1.openshiftapps.com/eshoprest/";
-     /*protected $client_id = "5b26340139e44b2a9bd4ee30";
-   protected $url_auth = "http://192.168.41.1:8081/eshop-auth-1.0-SNAPSHOT/";
-   protected $url_rest = "http://192.168.41.1:8081/eshoprest/";
+
  protected $client_id = "5b26340139e44b2a9bd4ee30";
  protected $url_auth = "https://nameless-tor-45908.herokuapp.com/";
  protected $url_rest = "https://desolate-castle-82146.herokuapp.com/";
